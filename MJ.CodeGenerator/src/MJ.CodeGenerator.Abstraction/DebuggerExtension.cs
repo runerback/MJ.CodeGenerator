aa0cc57700d8f0ef2c60d273cd94be4093ec815e
runerback <runerback@hotmail.com>
2023-08-11 17:08:25 +0800

    MJ.CodeGenerator.Abstraction.1.0.3
    Add `WaitForDebugger`. JIT debugger could not attach to generator, need attach to host process.

--- MJ.CodeGenerator/src/MJ.CodeGenerator.Abstraction/DebuggerExtension.cs
+++ MJ.CodeGenerator/src/MJ.CodeGenerator.Abstraction/DebuggerExtension.cs
@@ -1,16 +1,55 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace MJ.CodeGenerator
 {
     public static class DebuggerExtension
     {
+        public const int DefaultWaitTimeout = 30 * 1000;
+        public const int MinWaitTimeout = 1000;
+
         [DebuggerStepThrough]
-        public static void TryLaunchDebugger(this object _)
+        public static void TryLaunchDebugger(this object? _)
         {
             if (!Debugger.IsAttached)
             {
                 Debugger.Launch();
             }
+        }
+
+        /// <summary>
+        /// Wait for any debugger to attach.
+        /// </summary>
+        /// <param name="_"></param>
+        /// <param name="timeout">in Millisecond</param>
+        [DebuggerStepThrough]
+        public static async Task WaitForDebugger(this object? _, int timeout = DefaultWaitTimeout)
+        {
+            if (Debugger.IsAttached || timeout < MinWaitTimeout)
+            {
+                return;
+            }
+
+            await Task.WhenAny(
+                ((Func<Task>)(async () =>
+                {
+                    while (true)
+                    {
+                        if (Debugger.IsAttached)
+                        {
+                            break;
+                        }
+
+                        await Task.Delay(1000);
+                    }
+                }))(),
+                ((Func<Task>)(async () =>
+                {
+                    await Task.Delay(timeout);
+
+                    throw new TimeoutException();
+                }))());
         }
     }
 }
